398b6e62b8f665420a8eebbdd6a283595f63545c
Muhammad_Aqil_Rasyid_Permana <aqilrasyid2004@gmail.com>
2025-02-13 15:18:56 +0700

    feat: Add file model, factory, and controller for feature project header and project detail

---
+++ app/Http/Controllers/Api/V1/Project/ProjectHeaderController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Project;
+
+use App\Http\Controllers\Controller;
+use App\Models\ProjectHeader;
+use Illuminate\Http\Request;
+
+class ProjectHeaderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ProjectHeader $projectHeader)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ProjectHeader $projectHeader)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProjectHeader $projectHeader)
+    {
+        //
+    }
+}
---
+++ app/Models/ProjectHeader.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProjectHeader extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProjectHeaderFactory> */
+    use HasFactory;
+}
