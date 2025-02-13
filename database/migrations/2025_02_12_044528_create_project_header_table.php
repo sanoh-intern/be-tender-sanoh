ce40c01680a34223472c704d2526e42c5418dbc7
Muhammad_Aqil_Rasyid_Permana <aqilrasyid2004@gmail.com>
2025-02-13 15:12:38 +0700

    feat: add migration project_header and project_detail table

---
+++ database/migrations/2025_02_12_044528_create_project_header_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('project_header', function (Blueprint $table) {
+            $table->id();
+            $table->string('project_name',255)->nullable();
+            $table->string('project_status',25)->nullable();
+            $table->string('project_type',25)->nullable();
+            $table->text('project_description')->nullable();
+            $table->string('project_attach',255)->nullable();
+            $table->string('project_winner',255)->nullable();
+            $table->string('registration_status',25)->nullable();
+            $table->dateTime('registration_due_at')->nullable();
+            $table->unsignedBigInteger('final_review_by')->nullable();
+            $table->foreign('final_review_by')->references('id')->on('user')->onDelete('cascade');
+            $table->timestamp('final_review_at')->nullable();
+            $table->unsignedBigInteger('created_by')->nullable();
+            $table->foreign('created_by')->references('id')->on('user')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('project_header');
+    }
+};
---
+++ database/migrations/2025_02_13_113455_create_project_detail_table.php
@@ -0,0 +1,37 @@
+e<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('project_detail', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('supplier_id')->nullable();
+            $table->foreign('supplier_id')->references('id')->on('user')->onDelete('cascade');
+            $table->string('proposal_report',255)->nullable();
+            $table->bigInteger('proposal_total_amount')->nullable();
+            $table->string('proposal_status',25)->nullable();
+            $table->text('proposal_comment')->nullable();
+            $table->string('proposal_revision_no',25)->nullable();
+            $table->unsignedBigInteger('reviewed_by')->nullable();
+            $table->foreign('reviewed_by')->references('id')->on('user')->onDelete('cascade');
+            $table->timestamp('reviewed_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('project_detail');
+    }
+};
