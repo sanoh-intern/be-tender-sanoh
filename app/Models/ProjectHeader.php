9c5dd257df4fa0031f530f11ea728142dbb13703
Muhammad_Aqil_Rasyid_Permana <aqilrasyid2004@gmail.com>
2025-02-13 15:25:01 +0700

    feat: configure ProjectHeader model

--- app/Models/ProjectHeader.php
+++ app/Models/ProjectHeader.php
@@ -9,4 +9,29 @@ class ProjectHeader extends Model
 {
     /** @use HasFactory<\Database\Factories\ProjectHeaderFactory> */
     use HasFactory;
+
+    // Connection
+    protected $connection = 'mysql';
+
+    // Table name
+    protected $table = 'project_header';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'project_name',
+        'project_status',
+        'project_type',
+        'project_description',
+        'project_attach',
+        'project_winner',
+        'registration_status',
+        'registration_due_at',
+        'final_review_by',
+        'final_review_at',
+        'created_by',
+    ];
 }
