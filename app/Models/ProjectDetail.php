680f7fd289c2c4273c6087709994a5e92348f0b5
Muhammad_Aqil_Rasyid_Permana <aqilrasyid2004@gmail.com>
2025-02-13 15:29:40 +0700

    feat: configure ProjectDetail model

--- app/Models/ProjectDetail.php
+++ app/Models/ProjectDetail.php
@@ -9,4 +9,27 @@ class ProjectDetail extends Model
 {
     /** @use HasFactory<\Database\Factories\ProjectDetailFactory> */
     use HasFactory;
+
+    // Connection
+    protected $connection = 'mysql';
+
+    // Table name
+    protected $table = 'project_detail';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'project_header_id',
+        'supplier_id',
+        'proposal_report',
+        'proposal_total_amount',
+        'proposal_status',
+        'proposal_comment',
+        'proposal_revision_on',
+        'reviewed_by',
+        'reviewed_at',
+    ];
 }
