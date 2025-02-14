cacbdaa83ef61bbf3f0775441e581ae0390da104
Muhammad_Aqil_Rasyid_Permana <aqilrasyid2004@gmail.com>
2025-02-14 08:03:19 +0700

    feat: add AuthorizationRole traits

---
+++ app/AuthorizationRole.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Support\Facades\Auth;
+
+trait AuthorizationRole
+{
+    /**
+     * For role cheking.
+     * When you need to check what role have the access to the function
+     * @param string $role
+     */
+    public function permissibleRole(string $role) {
+        $role = Auth::user()->role->pluck('role_tag');
+
+        $check = $role->contains($role);
+
+        return $check;
+    }
+}
