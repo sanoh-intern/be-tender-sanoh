6545bc6662dfc2b643489d45a89da69f6e05c2d1
Muhammad_Aqil_Rasyid_Permana <aqilrasyid2004@gmail.com>
2025-02-14 09:01:51 +0700

    refactor: add code docummentation in AuthorizationRole trait

--- app/AuthorizationRole.php
+++ app/AuthorizationRole.php
@@ -8,7 +8,8 @@ trait AuthorizationRole
 {
     /**
      * For role cheking.
-     * When you need to check what role have the access to the function
+     * When you need to check what role have the access to the function.
+     * can accept more than one role using commas symbol. ex: permissibleRole('a','b');
      * @param string $role
      */
     public function permissibleRole(string ...$role) {
