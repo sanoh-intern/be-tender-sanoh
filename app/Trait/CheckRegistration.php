6dca887644eba99b0b88cd89c689c7f58aa9090b
Muhammad_Aqil_Rasyid_Permana <aqilrasyid2004@gmail.com>
2025-02-23 19:19:51 +0700

    feat: add docblock for isRegis method in CheckRegistration trait to improve code documentation

--- app/Trait/CheckRegistration.php
+++ app/Trait/CheckRegistration.php
@@ -6,6 +6,12 @@ use App\Models\User;
 
 trait CheckRegistration
 {
+    /**
+     * Checking user if already join the project
+     * @param int $userId
+     * @param int $projectId
+     * @return bool
+     */
     public function isRegis(int $userId, int $projectId) : bool {
         return User::whereHas('userProject', function ($query) use ($projectId) {
             $query->where('project_header_id', $projectId);
