d2f751f80e641db7c1b34bf12f101cd34b9d5568
Muhammad_Aqil_Rasyid_Permana <aqilrasyid2004@gmail.com>
2025-02-26 13:47:48 +0700

    fix: correct proposal_status assignment to handle string 'true' for final status

--- app/Http/Controllers/Api/V1/Project/ProjectDetailController.php
+++ app/Http/Controllers/Api/V1/Project/ProjectDetailController.php
@@ -82,7 +82,7 @@ class ProjectDetailController extends Controller
             'supplier_id' => $user,
             'proposal_attach' => $filePath,
             'proposal_total_amount' => $request->proposal_total_amount,
-            'proposal_status' => $request->proposal_status == true ? 'Final' : null ?? null,
+            'proposal_status' => $request->proposal_status == 'true' ? 'Final' : null ?? null,
             'proposal_revision_no' => $countSubmittedProposal,
         ]);
 
