b487d9d51b2c57ca4942581cada51d2d59dd0296
Muhammad_Aqil_Rasyid_Permana <aqilrasyid2004@gmail.com>
2025-05-25 20:14:42 +0700

    fix: correct column type for status in password_reset_tokens migration

--- database/migrations/2025_04_16_145836_add_status_to_password_reset_tokens_table.php
+++ database/migrations/2025_04_16_145836_add_status_to_password_reset_tokens_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('password_reset_tokens', function (Blueprint $table) {
-            $table->b('status')->default(false)->after('token');
+            $table->tinyInteger('status')->default(false)->after('token');
         });
     }
 
