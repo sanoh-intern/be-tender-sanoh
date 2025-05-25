c3e236fe6d7ef28b4f452f6395d7bd83ce0637a0
Muhammad_Aqil_Rasyid_Permana <aqilrasyid2004@gmail.com>
2025-05-25 20:20:49 +0700

    fix: standardize table name casing for user in migration to move company_photo

--- database/migrations/2025_04_17_115017_move_company_profile_from_user_table_to_company_profile_table.php
+++ database/migrations/2025_04_17_115017_move_company_profile_from_user_table_to_company_profile_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('User', function (Blueprint $table) {
+        Schema::table('user', function (Blueprint $table) {
             $table->dropColumn('company_photo');
         });
 
@@ -29,7 +29,7 @@ return new class extends Migration
             $table->dropColumn('company_photo');
         });
 
-        Schema::table('User', function (Blueprint $table) {
+        Schema::table('user', function (Blueprint $table) {
             $table->string('company_photo', 255)->after('role_id');
         });
 
