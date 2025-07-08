f5e838ef9e15b03ea6b70d86e9818965d4236825
Muhammad_Aqil_Rasyid_Permana <aqilrasyid2004@gmail.com>
2025-07-08 10:43:40 +0700

    fix: remove debug statement from getListProjectDetail method in ProjectDetailController

--- app/Http/Controllers/Api/V1/Project/ProjectDetailController.php
+++ app/Http/Controllers/Api/V1/Project/ProjectDetailController.php
@@ -45,7 +45,6 @@ class ProjectDetailController extends Controller
         if ($data->isEmpty()) {
             return $this->returnResponseApi(true, 'There is no proposal submitted.', '', 200);
         }
-        dd($data);
 
         return $this->returnResponseApi(
             true,
